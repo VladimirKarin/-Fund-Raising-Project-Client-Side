adfa08df63c38b4bd9827950252373e56ae727b5
Vladimir Karin <Vladimir.Karin@gmail.com>
2023-08-11 16:12:33 +0300

    FR-84 Create card component

---
+++ src/components/IdeaCardComponent.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea, CardActions } from '@mui/material';
+import ShowMoreButton from './Buttons/ShowMore';
+
+function IdeaCard(props) {
+    const { idea } = props;
+
+    // Null checks
+    if (!idea) return null;
+
+    const description = idea.description.slice(0, 150); // Limit length of the description here
+    const altText = `picture of ${idea.header}`;
+
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={idea.picture}
+                    alt={altText}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {idea.header}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <ShowMoreButton idea={idea.id} />{' '}
+            </CardActions>
+        </Card>
+    );
+}
+export default IdeaCard;
