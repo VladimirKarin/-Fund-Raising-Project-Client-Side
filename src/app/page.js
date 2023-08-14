35bc111af4547fee9ead89e4039370242150f5c2
Vladimir Karin <Vladimir.Karin@gmail.com>
2023-08-14 14:41:08 +0300

    FR-93 new progress bar variant

--- src/app/page.js
+++ src/app/page.js
@@ -7,14 +7,28 @@ import { LinearProgress } from '@mui/material';
 import IdeasDonationProgressBar from '@/components/ProgressBar';
 
 const idea = {
-    id: 'b8f40262-d265-419d-a769-e0299901b73b',
+    id: '70c8e131-ac49-453f-85f6-d18160b7be19',
     picture: './img/default_idea.png',
-    header: 'HEADER Nr. 1',
-    description: 'DESCRIPTION Nr. 1',
-    askedSum: 2500,
+    header: 'Idea 1',
+    description: 'Idea 1 Description 1 ',
+    askedSum: 100,
     userId: '6fba75be-1f13-449e-b8ee-0d9287d70208',
-    status: 'pending',
-    totalSum: 2000,
+    status: 'accepted',
+    donations: [
+        {
+            id: 'd6667fd3-0182-4730-9f20-866602f87ff8',
+            sum: 20,
+            userId: '6fba75be-1f13-449e-b8ee-0d9287d70208',
+            ideaId: '70c8e131-ac49-453f-85f6-d18160b7be19',
+        },
+        {
+            id: '7a739df1-7629-4743-8387-0334af332692',
+            sum: 10,
+            userId: '6fba75be-1f13-449e-b8ee-0d9287d70208',
+            ideaId: '70c8e131-ac49-453f-85f6-d18160b7be19',
+        },
+    ],
+    totalDonationSum: 30,
 };
 const HomePage = () => {
     return (
--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,33 +1,55 @@
 'use client';
+
 import React, { useState, useEffect } from 'react';
 import LinearProgress from '@mui/material/LinearProgress';
-import PropTypes from 'prop-types';
+import Typography from '@mui/material/Typography';
+
+import '../styles/IdeasDonationProgressBar.css';
 
 const IdeasDonationProgressBar = ({ idea }) => {
     const [progress, setProgress] = useState(0);
 
     useEffect(() => {
         if (idea.askedSum !== 0) {
-            const calculatedProgress = (idea.totalSum / idea.askedSum) * 100;
+            const calculatedProgress =
+                (idea.totalDonationSum / idea.askedSum) * 100;
             setProgress(calculatedProgress);
         } else {
             setProgress(0);
         }
-    }, [idea.askedSum, idea.totalSum]);
+    }, [idea.askedSum, idea.totalDonationSum]);
+
+    const contributors = idea.donations.length;
+    const remainingAmount = idea.askedSum - idea.totalDonationSum;
 
     return (
-        <LinearProgress
-            variant="determinate"
-            value={Math.min(progress, 100)} // Ensure the progress doesn't exceed 100%
-        />
-    );
-};
+        <div className="progress-bar-container">
+            {/* Total donated amount */}
+            <Typography variant="body2" align="center">
+                {`$${idea.totalDonationSum} Donated so far`}
+            </Typography>
 
-IdeasDonationProgressBar.propTypes = {
-    idea: PropTypes.shape({
-        askedSum: PropTypes.number.isRequired, // Target amount
-        totalSum: PropTypes.number.isRequired, // Cumulative donated amount
-    }).isRequired,
+            {/* Progress percentage and goal */}
+            <Typography variant="body2" align="center">
+                {`${progress.toFixed(0)}% of $${idea.askedSum} goal`}
+            </Typography>
+
+            {/* Progress bar */}
+            <LinearProgress
+                variant="determinate"
+                value={Math.min(progress, 100)}
+            />
+
+            {/* Contributors and remaining amount */}
+            <Typography variant="body2" align="center">
+                {`${contributors} contributors ${
+                    remainingAmount > 0
+                        ? `$${remainingAmount} to go`
+                        : 'Goal reached'
+                }`}
+            </Typography>
+        </div>
+    );
 };
 
 export default IdeasDonationProgressBar;
